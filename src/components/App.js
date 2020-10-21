682687dab5abe3464aabde3e1c3acb8040d7b014
sjsumit <46019235+sjsumit@users.noreply.github.com>
2020-10-21 22:04:21 +0530

    dddd

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showPara, setShowPara] = React.useState(false);
+  const handleClick = function () {
+    setShowPara(true);
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        Click
+      </button>
+      {showPara ? <Para /> : null}
     </div>
   );
 }
-
 
 export default App;
